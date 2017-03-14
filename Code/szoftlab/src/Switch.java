919a533bb750417ea64df0d113cb653c5e12f566
Szilard Domotor <dom.szilard@gmail.com>
2017-03-14 21:43:07 +0100

    szkeleton: Switxh routing implemented

--- Code/szoftlab/src/Switch.java
+++ Code/szoftlab/src/Switch.java
@@ -2,20 +2,29 @@
  * Created by Istvan Telek on 3/14/2017.
  */
 public class Switch extends Node {
+    private Node activeNode;
 
     public Switch() {
-    System.out.println("Switch.Switch");
-}
+        super();
+        System.out.println("Switch.Switch");
+        activeNode = null;
+    }
 
     @Override
     protected Node route() {
         System.out.println("Switch.route");
-        return new Node();
+        return activeNode;
     }
 
     @Override
     public void activate() {
         System.out.println("Switch.activate");
         boolean trainOnMe = checkForTrain();
+    }
+
+    @Override
+    public void addNeighbourNode(Node n) {
+        super.addNeighbourNode(n);
+        activeNode = n;
     }
 }
