1d62315f8a682e674fecab9f2817c629954dbda0
Szilard Domotor <dom.szilard@gmail.com>
2017-03-14 20:33:02 +0100

    szkeleton: TrainPart sq diagrams implemented

--- Code/szoftlab/src/TrainPart.java
+++ Code/szoftlab/src/TrainPart.java
@@ -1,5 +1,9 @@
 import util.Coordinate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Istvan Telek on 3/14/2017.
  */
@@ -10,31 +14,41 @@ public abstract class TrainPart {
     private Train train;
 
     public TrainPart(Train t) {
+        System.out.println("TrainPart.TrainPart");
         train = t;
     }
 
-    public void move() {
-
-    }
+    public abstract void move();
 
     public void setNextNode(Node n) {
+        System.out.println("TrainPart.setNextNode");
         prevNode = nextNode;
         nextNode = n;
     }
 
     public Train getTrain() {
+        System.out.println("TrainPart.getTrain");
         return train;
     }
 
     public boolean checkCollision(TrainPart tp) {
+        System.out.println("TrainPart.checkCollision");
+        System.out.println("[?] Collision happened?");
+        try {
+            return new BufferedReader(new InputStreamReader(System.in)).readLine().equals("Y");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     public Node getPrevNode() {
+        System.out.println("TrainPart.getPrevNode");
         return prevNode;
     }
 
     public Node getNextNode() {
-        return  nextNode;
+        System.out.println("TrainPart.getNextNode");
+        return nextNode;
     }
 }
