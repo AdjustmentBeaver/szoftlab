709bac9ad1d66294b33400ec07ad0ac2aa14cfb5
Szilard Domotor <dom.szilard@gmail.com>
2017-03-14 22:05:56 +0100

    szkeleton: SimulationTimer added subscriber list and step()

--- Code/szoftlab/src/SimulationTimer.java
+++ Code/szoftlab/src/SimulationTimer.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Istvan Telek on 3/14/2017.
  */
 public class SimulationTimer {
+    List<Notifiable> subscribers;
+
     public SimulationTimer() {
         System.out.println("SimulationTimer.SimulationTimer");
+        subscribers = new ArrayList<>();
     }
 
     public void start() {
@@ -14,11 +20,20 @@ public class SimulationTimer {
         System.out.println("SimulationTimer.stop");
     }
 
+    public void step() {
+        System.out.println("SimulationTimer.step");
+        for (Notifiable sub : subscribers) {
+            sub.update();
+        }
+    }
+
     public void addSubscriber(Notifiable sub) {
         System.out.println("SimulationTimer.addSubscriber");
+        subscribers.add(sub);
     }
 
     public void deleteSubscriptions() {
         System.out.println("SimulationTimer.deleteSubscriptions");
+        subscribers.clear();
     }
 }
