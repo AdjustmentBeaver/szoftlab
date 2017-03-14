d665fb4ed60d9e6e8e5bdce14352e4eb49f43cb1
Szilard Domotor <dom.szilard@gmail.com>
2017-03-14 19:58:11 +0100

    szkeleton: Train sq diagrams implemented

--- Code/szoftlab/src/Train.java
+++ Code/szoftlab/src/Train.java
@@ -1,4 +1,5 @@
 import util.Color;
+
 import java.util.List;
 
 /**
@@ -11,37 +12,58 @@ public class Train implements Notifiable {
     private boolean isRunning;
 
     public Train(Statistics st, List<Train> trainList) {
+        System.out.println("Train.Train");
         stat = st;
         this.trainList = trainList;
         isRunning = false;
     }
 
     public void move() {
-
+        System.out.println("Train.move");
+        for (TrainPart tp : trainPartList) {
+            tp.move();
+        }
     }
 
     public void explode() {
-
+        System.out.println("Train.explode");
+        stat.trainExploded();
     }
 
     public boolean isRunning() {
+        System.out.println("Train.isRunning");
         return isRunning;
     }
 
     public void addPart(TrainPart p) {
-
+        System.out.println("Train.addPart");
+        trainPartList.add(p);
     }
 
     public Color getColor() {
+        System.out.println("Train.getColor");
         return new Color("");
     }
 
     public void startTrain() {
+        System.out.println("Train.startTrain");
         isRunning = true;
     }
 
     public void checkCollision() {
-
+        System.out.println("Train.checkCollision");
+        for (Train other : trainList) {
+            List<TrainPart> othParts = other.getPartList();
+            if (other != this) {
+                for (TrainPart otherPart : othParts) {
+                    for (TrainPart ownPart : trainPartList) {
+                        boolean collided = otherPart.checkCollision(ownPart);
+                        if (collided)
+                            explode();
+                    }
+                }
+            }
+        }
     }
 
     public List<TrainPart> getPartList() {
@@ -50,6 +72,7 @@ public class Train implements Notifiable {
 
     @Override
     public void update() {
-
+        move();
+        checkCollision();
     }
 }
