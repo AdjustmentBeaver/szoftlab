e73e59ff5f0d14b7783ad0e3c4de75ec93db4181
Szilard Domotor <dom.szilard@gmail.com>
2017-03-14 20:26:13 +0100

    szkeleton: TrainCart sq diagrams implemented

--- Code/szoftlab/src/TrainCart.java
+++ Code/szoftlab/src/TrainCart.java
@@ -5,19 +5,23 @@ import util.Color;
  */
 public class TrainCart extends TrainPart {
     private Color color;
-    private Statistics stat;
+    private Statistics st;
 
     public TrainCart(Train t, Statistics st, Color color) {
         super(t);
+        System.out.println("TrainCart.TrainCart");
         this.color = color;
-        stat = st;
+        this.st = st;
     }
 
     public Color getColor() {
+        System.out.println("TrainCart.getColor");
         return color;
     }
 
     public void unload() {
-
+        System.out.println("TrainCart.unload");
+        color = new Color("empty");
+        st.cartUnloaded();
     }
 }
