9b9d34f843939cd4caf55fe9d20612843220103d
Szilard Domotor <dom.szilard@gmail.com>
2017-03-18 16:39:13 +0100

    szkeleton: MapManager comments

--- Code/szoftlab/src/MapManager.java
+++ Code/szoftlab/src/MapManager.java
@@ -2,17 +2,35 @@
  * Created by szilard95 on 3/14/17.
  * Project: szoftlab
  */
+
+/**
+ * A pályák kezeléséért felelős. <br>
+ * Képes menteni egy játékállást, betölteni egy korábban lementettet, vagy újat, és kezeli az ezzel járó
+ * feliratkoztatásokat (ld. subscription szekvencia). <br>
+ * Tárolja a jelenlegi Map-ot.
+ */
 public class MapManager {
     private Game game;
     private Map map;
     private SimulationTimer timer;
 
+    /**
+     * Instantiates a new Map manager.
+     *
+     * @param timer the timer
+     * @param game  the game
+     */
     public MapManager(SimulationTimer timer, Game game) {
         Prompt.printMessage("MapManager.MapManager");
         this.game = game;
         this.timer = timer;
     }
 
+    /**
+     * Betölti a paraméterül kapott nevű pályát egy MapBuilder segítségével.
+     *
+     * @param mapName the map name
+     */
     public void newMap(String mapName) {
         Prompt.printMessage("MapManager.newMap");
 
@@ -29,6 +47,11 @@ public class MapManager {
         Prompt.removeIndent();
     }
 
+    /**
+     * Lementi az aktuális játékállást a paraméterben kapott néven.
+     *
+     * @param mapName the map name
+     */
     public void saveMap(String mapName) {
         Prompt.printMessage("MapManager.saveMap");
 
@@ -42,6 +65,11 @@ public class MapManager {
         Prompt.removeIndent();
     }
 
+    /**
+     * Betölt egy korábban lementett játékállást.
+     *
+     * @param mapName the map name
+     */
     public void loadMap(String mapName) {
         Prompt.printMessage("MapManager.loadMap");
         // Deserialization
