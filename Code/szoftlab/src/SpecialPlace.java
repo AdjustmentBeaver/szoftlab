f9a66e26b5dd5a381cd022282054494dbdfe2b94
Andras Foldvari <fandras95@gmail.com>
2017-04-14 21:14:44 +0200

    proto: class: SpecialPlace

--- Code/szoftlab/src/SpecialPlace.java
+++ Code/szoftlab/src/SpecialPlace.java
@@ -1,16 +1,25 @@
+import java.util.List;
+
 /**
  * Created by Istvan Telek on 3/14/2017.
  * <p>
  *     Ezeken a speciális csomópontokon tud a felhasználó alagútszájakat építeni. A pályán maximum 2 alagútszáj lehet aktív, amiket alagút köt össze. Megépített alagútszájat le is lehet rombolni, ha nem tartózkodik az alagútban vonat.
  * </p>
  */
-public class SpecialPlace extends Node {
+public class SpecialPlace extends Node{
+
+    private static final int MAX_NUM_SPECIALPLACE = 2;
+
+    List<SpecialPlace> spList;
+    boolean isConstructed;
 
     /**
      * Konstruktor. Létrehozza a SpecialPlacet.
      */
-    public SpecialPlace() {
-        Prompt.printMessage("SpecialPlace.SpecialPlace");
+    public SpecialPlace(List<SpecialPlace> spList) {
+        super();
+        this.spList = spList;
+        isConstructed = false;
     }
 
     /**
@@ -18,8 +27,32 @@ public class SpecialPlace extends Node {
      */
     @Override
     public void activate() {
-        Prompt.addIndent("tunnel.checkForTrain()");
         boolean trainOnMe = checkForTrain();
-        Prompt.removeIndent();
+        boolean canConstruct = canConstruct();
+        if (!trainOnMe && canConstruct) {
+            isConstructed = true;
+        }
+    }
+
+    @Override
+    protected Node route() {
+        // Ha van másik felépített alagútszáj
+        for(SpecialPlace sp: spList){
+            if (sp.isConstructed && sp != this)
+                return sp;
+        }
+        return null;
+    }
+
+    private boolean canConstruct(){
+        int numOfConstructed = 0;
+        for(SpecialPlace sp: spList)
+            if (sp.isConstructed) numOfConstructed++;
+
+        // Ha fel van építve az összes lehetséges
+        if (numOfConstructed == SpecialPlace.MAX_NUM_SPECIALPLACE)
+            return false;
+
+        return true;
     }
 }
