dc8c8b61dca5b3b47b88eb7116d0b6913770aac2
Andras Foldvari <fandras95@gmail.com>
2017-04-16 19:56:51 +0200

    proto: class: BoundingBox collisionCheck

--- Code/szoftlab/src/util/BoundingBox.java
+++ Code/szoftlab/src/util/BoundingBox.java
@@ -24,11 +24,11 @@ public class BoundingBox {
     private ArrayList<Coordinate> points;
 
     /**
-     *
+     * Konstruktor
      * @param middle középpont
      * @param direction irányvektor
      */
-    BoundingBox(Coordinate middle,Coordinate direction){
+    public BoundingBox(Coordinate middle,Coordinate direction){
         setPos(middle,direction);
     }
 
@@ -46,5 +46,88 @@ public class BoundingBox {
         points.add(2,new Coordinate(middle.getX()-direction.getX()-direction.getY()*0.5,middle.getY()-direction.getY()+direction.getX()*0.5));
         points.add(3,new Coordinate(middle.getX()-direction.getX()+direction.getY()*0.5,middle.getY()-direction.getY()-direction.getX()*0.5));
 
+    }
+
+    /**
+     * Visszaadja a BoundingBox pontjait
+     * @return
+     */
+    public ArrayList<Coordinate> getPoints() {
+        return points;
+    }
+
+    /**
+     * Összehasonlítja, hogy a paraméterben kapott BoundigBox-al ütközött-e
+     * @param otherBoundingBox A másik BoundingBox amivel az ütközést vizsgáljuk
+     * @return  Történt-e ütközés
+     */
+    public boolean isCollided(BoundingBox otherBoundingBox){
+        int numOfOverLaps = 0;
+
+        // For A axises
+        for (int i = 0; i<2; i++){
+            Coordinate axis1 = new Coordinate(points.get(i+1).getX() - points.get(i).getX(),
+                                                points.get(i+1).getY() - points.get(i).getY());
+
+            if (checkAxisCollision(otherBoundingBox, axis1))
+                numOfOverLaps++;
+            else
+                return false;
+        }
+
+        // For B axises
+        for (int i = 0; i<2; i++){
+            Coordinate axis1 = new Coordinate(otherBoundingBox.getPoints().get(i+1).getX() - otherBoundingBox.getPoints().get(i).getX(),
+                                                otherBoundingBox.getPoints().get(i+1).getY() - otherBoundingBox.getPoints().get(i).getY());
+
+            if (checkAxisCollision(otherBoundingBox, axis1))
+                numOfOverLaps++;
+            else
+                return false;
+        }
+
+        if (numOfOverLaps == 4)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Megnézi egy adott irányra van-e átfedés
+     * @param otherBoundingBox A másik BoundingBox amivel az ütközést vizsgáljuk.
+     * @param axis Az irány, amiben vizsgáljuk az ütközést
+     * @return Történt-e ütközés
+     */
+    private boolean checkAxisCollision(BoundingBox otherBoundingBox, Coordinate axis){
+        double Amax, Amin, Bmax, Bmin;
+
+        // A MAX, MIN meghatározása
+        Amax = Amin = getScalarForCollision(points.get(0), axis);
+
+        for(int i = 1; i<points.size(); i++){
+            double scalar = getScalarForCollision(points.get(i), axis);
+            if (scalar < Amin) Amin = scalar;
+            if (scalar > Amax) Amax = scalar;
+        }
+
+        // B MAX, MIN
+        Bmax = Bmin = getScalarForCollision(otherBoundingBox.getPoints().get(0), axis);
+
+        for(int i = 1; i<points.size(); i++){
+            double scalar = getScalarForCollision(otherBoundingBox.getPoints().get(i), axis);
+            if (scalar < Bmin) Bmin = scalar;
+            if (scalar > Bmax) Bmax = scalar;
+        }
+
+        if (Bmin <= Amin || Bmax >= Amax)
+            return true;
+
+        return false;
+    }
+
+    private double getScalarForCollision(Coordinate point, Coordinate axis){
+        double projConst = (point.getX() * axis.getX() + point.getY() * axis.getY())/(Math.pow(axis.getX(),2) + Math.pow(axis.getY(),2));
+        double pointScalar = projConst * Math.pow(point.getX(),2) + projConst * Math.pow(point.getY(),2);
+        return pointScalar;
     }
 }
